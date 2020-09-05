9f66c6af00411d721d624da68607a3a2bd3993fe
AxelSoto <axelmlsoto@gmail.com>
2020-09-04 23:35:10 -0400

    Add files via upload

---
+++ Practica I.py
@@ -0,0 +1,83 @@
+print("-----------------------------------------------------------------------------------------------------------------------------------")
+print("Ejercicio 1 ")
+print("Respuesta: Tipo de dato:(falso)")
+
+print("Ejercicio 2")
+
+nombre ="Axel"
+
+print("Bienvenido "+ nombre)
+
+print("---------------------------------------------------------------------------------------------------------------------------------")
+print("Ejercicio 3")
+numero = input("Escriba el numero:")
+res = int(numero) % 2 
+
+if res == 0:
+    print("su numero es par")
+
+else:
+     print("Su numero es impar")
+
+print("-------------------------------------------------------------------------------------------------------------------------------")
+print("ejercicio 4")
+a = 3
+
+b = 8
+
+if a > b:
+    print("Su resultado es verdadero")
+else:
+    print("Su resultado es falso")
+
+print("----------------------------------------------------------------------")
+print("ejercicio 5")
+Cantidad_dollar = input("ingrse cantidad dollar: ")
+Cantidad_dollar = float(Cantidad_dollar)
+tasa = 58.43
+
+
+
+
+print("Es igual a ")
+print(Cantidad_dollar * tasa)
+
+
+print("----------------------------------------------------------------------------------------------------------------------------------")
+print("es para separar!")
+print("ejercicio 6")
+
+
+grado_far = 33.8
+
+Cantidad_celcius = input("Ingrese Cantidad de grados celsius:")
+Cantidad_celcius = float(Cantidad_celcius)
+
+print("Grados Fahrenheit:")
+print(Cantidad_celcius * 9 / 5 +32)
+
+print("---------------------------------------------------------------------------------")
+print("ejercicio 7")
+
+Axel_Matematiacas = 90
+Axel_Sociales = 77
+Axel_Programacion = 95
+Axel_Español = 92
+
+total1 = Axel_Sociales + Axel_Matematiacas + Axel_Programacion + Axel_Español
+
+print(total1 / 4)
+
+print("----------------------------------------------------------------------------------------------------")
+print("Ejercicio 8(final)")
+
+Monto = 1000
+Cuotas = 12
+interes = 5
+
+print( interes  + 1 ** Cuotas - 1 - Monto  )
+
+print("Gracias por todo")
+
+
+
