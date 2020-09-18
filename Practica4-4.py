c76b91d990d5b686779ab2dc1e578093d69f8001
AxelSoto <axelmlsoto@gmail.com>
2020-09-17 22:18:50 -0400

    Add files via upload

---
+++ Practica4-4.py
@@ -0,0 +1,28 @@
+class personaje:
+    def mover_arriba(self):
+        pass
+
+    def mover_abajo(self):
+        pass
+
+    def mover_derecha(self):
+        pass
+
+    def mover_izquierda(self):
+        pass
+
+
+class Mario(personaje):
+    personaje = personaje()
+    personaje.mover_arriba()
+    personaje.mover_abajo()
+    personaje.mover_derecha()
+    personaje.mover_izquierda()
+
+
+class Koopa(personaje):
+    personaje = personaje()
+    personaje.mover_arriba()
+    personaje.mover_abajo()
+    personaje.mover_derecha()
+    personaje.mover_izquierda()
